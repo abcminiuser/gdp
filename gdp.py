47fa70c0ed1fcfa6612da96c56dd9bb3abb8f00d
Dean Camera <dean@fourwalledcubicle.com>
2013-10-27 17:16:17 +0100

    Initial commit.

---
+++ gdp.py
@@ -0,0 +1,12 @@
+'''
+            GDP - The Generic Device Programmer.
+
+    By Dean Camera (dean [at] fourwalledcubicle [dot] com)
+'''
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+    main()
