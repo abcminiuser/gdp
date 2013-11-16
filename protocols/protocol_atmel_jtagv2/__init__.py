d924f3c6d009fea5be0cdd07d6c536882658a809
Dean Camera <dean@fourwalledcubicle.com>
2013-11-16 20:54:08 +0100

    JTAGV2 Protocol: Move into subdirectory.

---
+++ protocols/protocol_atmel_jtagv2/__init__.py
@@ -0,0 +1,7 @@
+'''
+            GDP - The Generic Device Programmer.
+
+    By Dean Camera (dean [at] fourwalledcubicle [dot] com)
+'''
+
+from protocols.protocol_atmel_jtagv2.atmel_jtagv2 import *
